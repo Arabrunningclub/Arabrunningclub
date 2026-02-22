44d7084ece459c62526cec419781eb08638a11b6
SilkRoadRunners28 <arabrunningclub@gmail.com>
2026-02-22 10:22:05 -0500

    removed about link on nav

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,6 @@ import { AnimatePresence, motion } from "framer-motion"
 
 const navItems = [
   { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
   { name: "Events", href: "/events" },
   { name: "Charity", href: "/charity" },
   { name: "Donations", href: "/donations" },
